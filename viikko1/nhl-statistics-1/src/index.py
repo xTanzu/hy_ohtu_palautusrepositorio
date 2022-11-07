836d550ef58f71cae521b92bb30fa38bcb7b59b4
Taneli Härkönen <taneliharkonen@hotmail.com>
2022-11-07 18:33:19 +0200

    Tee tehtävään 15 asti

---
+++ viikko1/nhl-statistics-1/src/index.py
@@ -0,0 +1,20 @@
+from player_reader import PlayerReader
+from statistics import Statistics
+
+def main():
+    reader = PlayerReader()
+    stats = Statistics(reader)
+    philadelphia_flyers_players = stats.team("PHI")
+    top_scorers = stats.top(10)
+
+    print("Philadelphia Flyers:")
+    for player in philadelphia_flyers_players:
+        print(player)
+
+    print("Top point getters:")
+    for player in top_scorers:
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
