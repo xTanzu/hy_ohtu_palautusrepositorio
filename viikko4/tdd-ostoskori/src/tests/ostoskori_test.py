998c4f5e6df3da8fb3b18d0e4bbe314fe70df1c1
Taneli Härkönen <taneliharkonen@hotmail.com>
2022-11-24 01:52:27 +0200

    Step 9: Yhden tuotteen lisäämisen jälkeen..
      - ostoskori sisältää ostoksen, jolla sama nimi kuin tuotteella ja lukumäärä 1

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -62,3 +62,11 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
         self.assertTrue(isinstance(ostokset[0], Ostos))
+
+    # step 9
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 1)
