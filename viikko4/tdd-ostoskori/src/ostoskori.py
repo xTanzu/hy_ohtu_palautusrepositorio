a6370803c8676a3801ec5bede0203900867ba138
Taneli Härkönen <taneliharkonen@hotmail.com>
2022-11-23 20:57:28 +0200

    Step 1: Testaa että ostoskorin tavaroiden määrä ja hinta on alussa 0

---
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,30 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        pass
+        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+
+    def tavaroita_korissa(self):
+        return 0
+
+    def hinta(self):
+        return 0
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        # lisää tuotteen
+        pass
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        pass
+
+    def tyhjenna(self):
+        pass
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        pass
+        # palauttaa listan jossa on korissa olevat ostos-oliot
+        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
